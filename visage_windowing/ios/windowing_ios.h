7b394bfe66831a6c58af4a52504dda9d2ad7d111
Dan Raffel <daniel.raffel@gmail.com>
2026-03-02 17:08:19 -0800

    Add iOS windowing implementation
    Full WindowIos implementation with:
    - MTKView + CAMetalLayer rendering at 60fps
    - Touch to mouse event mapping (single primary touch)
    - Factory functions: createWindow, createPluginWindow
    - Platform utilities: isMobileDevice, defaultDpiScale,
      computeWindowBounds, clipboard, message box
    
    The view supports multipleTouchEnabled for future multi-touch
    support. Single-touch dispatch through the event system is
    handled in this PR.

---
+++ visage_windowing/ios/windowing_ios.h
@@ -0,0 +1,74 @@
+/* Copyright Vital Audio, LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#pragma once
+
+#if VISAGE_IOS
+#include "windowing.h"
+
+#import <UIKit/UIKit.h>
+#import <MetalKit/MetalKit.h>
+
+namespace visage {
+  class WindowIos;
+}
+
+@interface VisageMetalViewDelegate : NSObject <MTKViewDelegate>
+@property(nonatomic) visage::WindowIos* visage_window;
+@property long long start_microseconds;
+@end
+
+@interface VisageMetalView : MTKView
+@property(nonatomic) visage::WindowIos* visage_window;
+@property(nonatomic, strong) UITouch* active_touch;
+
+- (instancetype)initWithFrame:(CGRect)frame inWindow:(visage::WindowIos*)window;
+@end
+
+namespace visage {
+  class WindowIos : public Window {
+  public:
+    WindowIos(int width, int height, float scale);
+    WindowIos(int width, int height, float scale, void* parent_handle);
+    ~WindowIos() override;
+
+    void runEventLoop() override;
+    void* nativeHandle() const override { return (__bridge void*)view_; }
+    void* initWindow() const override;
+    void windowContentsResized(int width, int height) override;
+    void show() override;
+    void showMaximized() override;
+    void hide() override;
+    void close() override;
+    bool isShowing() const override;
+    void setWindowTitle(const std::string& title) override;
+    IPoint maxWindowDimensions() const override;
+
+    void handleNativeResize(int width, int height);
+
+  private:
+    UIView* parent_view_ = nullptr;
+    VisageMetalView* view_ = nil;
+    VisageMetalViewDelegate* view_delegate_ = nil;
+  };
+}
+
+#endif
